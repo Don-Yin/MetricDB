9b7fd259b806c194fe77b05399a849db876e1815
Don Yin <Don_Yin@outlook.com>
2024-11-18 21:27:12 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="MetricDB",
-    version="0.0.7",
+    version="0.0.8",
     author="Don Yin",
     author_email="Don_Yin@outlook.com",
     description="A logger based on SQLite3",
